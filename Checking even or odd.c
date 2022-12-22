4a07fddb24b2a8a45d3c4193c084c8afec32bed3
Moon2340 <farjanamoon20144@gmail.com>
2022-12-22 22:23:04 +0600

    checking evenodd number

---
+++ Checking even or odd.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int evenodd(int n1);
+int main()
+{
+    int number;
+    printf("Enter a number: ");
+    scanf("%d",&number);
+    evenodd(number);
+    if (evenodd(number)==1)
+        printf("The number is even");
+    else
+        printf("The number is odd");
+    return 0;
+}
+int evenodd(int n1)
+{
+    if (n1%2==0)
+        return 1;
+        else
+        return 0;
+}
